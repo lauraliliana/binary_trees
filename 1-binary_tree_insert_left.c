8f6843ff53499e8fdeab83a3ce42edf629b81bc0
lauraliliana <lauraliliana@gmail.com>
2021-07-22 23:00:08 -0500

    1

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,17 +1,32 @@
-# Binary Trees
-----
+#include "binary_trees.h"
+/**
+ * binary_tree_insert_left - inserts new node into left child of parent
+ * @parent: the parent node to add a node to
+ * @value: to be given to the new node
+ *
+ * Return: a pointer to new_node or NULL
+ */
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *node, *left_node;
 
-* What is a binary tree
-* What is the difference between a binary tree and a Binary Search Tree
-* What is the possible gain in terms of time complexity compared to linked lists
-* What are the depth, the height, the size of a binary tree
-* What are the different traversal methods to go through a binary tree
-* What is a complete, a full, a perfect, a balanced binary tree
+	if (parent)
+	{
+		left_node = parent->left;
+		node = malloc(sizeof(binary_tree_t) * 1);
 
-## Author
-
-Laura Valencia, [lauraliliana@gmail.com]
-
-## License
-
-MIT License
+		if (node)
+		{
+			node->parent = parent;
+			node->n = value;
+			node->right = NULL;
+			node->left = left_node;
+			if (left_node)
+				left_node->parent = node;
+		}
+		parent->left = node;
+	}
+	else
+		node = NULL;
+	return (node);
+}
